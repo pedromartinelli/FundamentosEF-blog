56e3f8987856e5fbfe2615cb6f3781e11c8f48b5
Pedro Martinelli <p.martinellinunes@gmail.com>
2024-01-24 10:57:50 -0300

    first commit - criando os modelos

--- Program.cs
+++ Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FundamentosEF_Blog
+﻿namespace FundamentosEF_Blog
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
