978edb0b1a3583135409fcdbe8a337c58a390300
Pedro Martinelli <p.martinellinunes@gmail.com>
2024-01-24 11:52:52 -0300

    configurando o datacontext

---
+++ Data/BlogDataContext.cs
@@ -0,0 +1,20 @@
+﻿using FundamentosEF_Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FundamentosEF_Blog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Server=localhost,1433;Database=sqlserver;User Id=sa;Password=1q2w3e!@#");
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
-﻿namespace FundamentosEF_Blog
+﻿using FundamentosEF_Blog.Data;
+
+namespace FundamentosEF_Blog
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            using var context = new BlogDataContext();
         }
     }
 }
